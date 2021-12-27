0a5e8e080ff0f15abd1a423ed265f8464b9eb35c
Nikita Kazakov <nikita.kazakov95@gmail.com>
2021-12-27 21:38:08 +0700

    Third commit.

--- ajsfhkjasTests/ajsfhkjasTests.swift
+++ ajsfhkjasTests/ajsfhkjasTests.swift
@@ -11,7 +11,6 @@ import XCTest
 class ajsfhkjasTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
