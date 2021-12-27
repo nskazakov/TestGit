17d243489650c03df176228824bab48e47e3231f
Nikita Kazakov <nikita.kazakov95@gmail.com>
2021-12-27 21:18:09 +0700

    Initial Commit

---
+++ ajsfhkjas/ajsfhkjasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ajsfhkjasApp.swift
+//  ajsfhkjas
+//
+//  Created by Nikita Kazakov on 27.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ajsfhkjasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
